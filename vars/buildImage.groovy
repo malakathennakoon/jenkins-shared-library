123c0d37fe7c990f4a983115fee66a34616dd4a3
malakathennakoon <malaka@MalakaT.localdomain>
2026-03-12 18:42:01 +0100

    adding parameter variable placeholders

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(string ImageName, string version) {
     echo "Building the Docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'echo "Logging in to Docker Hub..."'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker build -t shiranatdocker/demo-app:jma-1.0 .'
-        sh 'docker push shiranatdocker/demo-app:jma-1.0'
+        sh "docker build -t $ImageName-$version . "
+        sh "docker push $ImageName-$version"
     }
 }
